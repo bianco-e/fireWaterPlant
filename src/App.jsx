2966118f34ecb7b7e5402141b60793a90fceb04b
Bianco <bianco.emiliozzi@gmail.com>
2020-04-04 15:43:00 -0300

    set scores, and messages when winning or losing

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,83 @@ import options from "./data.js";
 function App() {
   const [userOption, setUserOption] = useState("");
   const [computerOption, setComputerOption] = useState("");
-  const [score, setScore] = useState({ User: 0, Computer: 0 });
+  const [userScore, setUserScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+
+  const emptyChosenOptions = () => {
+    setUserOption("");
+    setComputerOption("");
+  };
+
+  const restartScores = () => {
+    setUserScore(0);
+    setComputerScore(0);
+  };
+
+  const fire = options[0];
+  const water = options[1];
+  const plant = options[2];
 
   useEffect(() => {
-    if (userOption === options[0].content) {
-      computerOption === options[1].content
-        ? alert("You lost!")
-        : computerOption === options[2].content && alert("You won!");
-    } else if (userOption === options[1].content) {
-      computerOption === options[2].content
-        ? alert("You lost!")
-        : computerOption === options[0].content && alert("You won!");
-    } else {
-      computerOption === options[0].content
-        ? alert("You lost!")
-        : computerOption === options[1].content && alert("You won!");
+    if (userScore === 3) {
+      alert("You win!");
+      restartScores();
     }
+    if (computerScore === 3) {
+      alert("You lose!");
+      restartScores();
+    }
+  }, [userScore, computerScore]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (userOption === fire.content) {
+        if (computerOption === water.content) {
+          alert("Water turned you off!");
+          setComputerScore(computerScore + 1);
+          emptyChosenOptions();
+        } else if (computerOption === plant.content) {
+          alert("You burnt the plant!");
+          setUserScore(userScore + 1);
+          emptyChosenOptions();
+        } else {
+          emptyChosenOptions();
+        }
+      } else if (userOption === water.content) {
+        if (computerOption === plant.content) {
+          alert("Plant absorbed you!");
+          setComputerScore(computerScore + 1);
+          emptyChosenOptions();
+        } else if (computerOption === fire.content) {
+          alert("You turned fire off!");
+          setUserScore(userScore + 1);
+          emptyChosenOptions();
+        } else {
+          emptyChosenOptions();
+        }
+      } else if (userOption === plant.content) {
+        if (computerOption === fire.content) {
+          alert("Fire burnt you!");
+          setComputerScore(computerScore + 1);
+          emptyChosenOptions();
+        } else if (computerOption === water.content) {
+          alert("You absorbed the water!");
+          setUserScore(userScore + 1);
+          emptyChosenOptions();
+        } else {
+          emptyChosenOptions();
+        }
+      }
+    }, 400);
   }, [userOption, computerOption]);
 
   return (
     <div className="wrapper">
-      <div className="divToCenter">
-        <div>User</div>
-        <div>Computer</div>
-        <h4>{score.User}</h4>
-        <h4>{score.Computer}</h4>
+      <div className="scoreDiv">
+        <h4>User</h4>
+        <h4>Computer</h4>
+        <h4>{userScore}</h4>
+        <h4>{computerScore}</h4>
       </div>
       <div className="halfScreenDiv lightGrey">
         <div className="divToCenter">
